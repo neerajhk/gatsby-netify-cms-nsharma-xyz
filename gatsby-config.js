268984e715d49a9f15c5a0ef548c766c98c43483
neerajhk <60499452+neerajhk@users.noreply.github.com>
2021-12-12 15:55:22 +0800

    changes
    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "nsharma.xyz",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This website is a blog by Neeraj.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
